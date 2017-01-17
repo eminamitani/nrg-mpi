fa3b1330daf79423504c1e092bfed73348e1b69e
alpha_emitter <devnull@localhost>
2017-01-17 13:42:36 +0900

    change the loop index

--- invariantMatrix.F90
+++ invariantMatrix.F90
@@ -218,8 +218,8 @@ subroutine invariantMatrix(iteration)
             write(matrixnum,*) matrixkind
             open(110,file="itr_"//TRIM(ADJUSTL(Numitr))//"_invariant_matrix"//TRIM(ADJUSTL(matrixnum))//".txt")
 
-            do iright=1, hami%numberOfBasis
             do ileft=1, hami%numberOfBasis
+            do iright=1, hami%numberOfBasis
             write(110, *) invariant_matrix(iright,ileft,matrixkind)
             end do
             end do
